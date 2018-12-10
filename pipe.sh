ba79093953ee5d3831f09ecf2685397d0df87d97
John Gliksberg <jg.trosh@gmail.com>
2018-12-10 14:30:57 +0100

    Simulate pipe behaviour to commands that only take input from arguments

---
+++ pipe.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Simulate pipe behaviour to commands that only take input from arguments
+
+test $# -gt 0 || {
+	echo "Usage: <INPUT> | pipe cmd [ext]" 1>&2
+	exit 1
+}
+
+if test $# -eq 2
+then
+	ext=.$2
+else
+	case $1 in
+		surf | chrome ) ext=.html ;;
+		mupdf ) ext=.pdf ;;
+		* ) ext= ;;
+	esac
+fi
+
+case $1 in
+	chrome ) prefix="--app=file://" ;;
+	* ) prefix= ;;
+esac
+
+tmppipe=/tmp/pipe.$$$ext
+cat > $tmppipe # Redirect stdin to file
+$1 $prefix$tmppipe # Run command on file
